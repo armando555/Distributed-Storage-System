8366adcfd9862f3974ba7c01e7f09cb214648a9c
Armando Rios Gallego <armandosebas4@gmail.com>
2021-10-04 15:00:36 -0500

    client and server working in aws

--- client.py
+++ client.py
@@ -12,9 +12,15 @@ import cv2
 import random
 
 
+
+
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
+
+
 files = []
+
+
 
 class helloHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -22,13 +28,9 @@ class helloHandler(BaseHTTPRequestHandler):
         self.send_header('content_type', 'application/octet-stream')
         self.end_headers()
         file = self.path[1:]
-        if os.path.isfile(file):
-            d = open(self.path[1:],"rb")
-            file = d.read()
-            self.wfile.write(file)
-        else:
-            msg = "No, sorry I dont have it " + file
-            self.wfile.write(pickle.dumps(msg))
+        d = open(self.path[1:],"rb")
+        file = d.read()
+        self.wfile.write(file)
     def do_POST(self):
         print("ESTOY HACIENDO POST JEJE")
         content_len = int(self.headers.get('content-length'))
@@ -44,49 +46,58 @@ class helloHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         #self.wfile.write(bytes("okey","utf-8"))
-        
+
+
+
 
 
 def main():
-    PORT = 54000
-    server = HTTPServer(('localhost', PORT), helloHandler)
+    PORT = 53000
+    server = HTTPServer(('', PORT), helloHandler)
     print(f'server running on port {PORT}')
     threads = []
     tServer = threading.Thread(target=server.serve_forever)
     threads.append(tServer)
     tServer.start()
 
+
+
     print('***********************************')
     print('Client is running...')
-    client_socket.connect(("127.0.0.1",constants.PORT))
+    client_socket.connect((constants.IP_SERVER,constants.PORT))
     local_tuple = client_socket.getsockname()
     print('Connected to the server from:', local_tuple)
     print('Enter \"quit\" to exit')
     print('What do you want to do?\nDownload\nSave')
     command_to_send = input()
 
+
+
     while command_to_send != constants.QUIT:
         if command_to_send == '':
             print('Please input a valid command...')
-            command_to_send = input()                        
+            command_to_send = input()
         elif (command_to_send == constants.DOWNLOAD):
-            data_to_send = input('Input data to download: ') 
+            data_to_send = input('Input data to download: ')
             command_and_data_to_send = command_to_send + ' ' + data_to_send
             command_and_data_to_send = pickle.dumps(command_and_data_to_send)
             client_socket.send(command_and_data_to_send)
             data_received = client_socket.recv(constants.RECV_BUFFER_SIZE)
+            print("papi la recibi como es")
             d = pickle.loads(data_received)
+            #file = open(data_to_send,"w")
+            #file.write(d)
+            #file.close()
+            print(d)
+            print(type(d))
             cv2.imwrite(data_to_send,d)
-            cv2.imshow('Imagen',d)
-            cv2.waitKey(5000)
-            cv2.destroyAllWindows()
             print("200 OK")
             command_to_send = input()
         elif (command_to_send == constants.SAVE):
             data_to_send = input('Input data to save: ')
             name = data_to_send
-            files.append(data_to_send) 
-            d = cv2.imread(data_to_send) 
+            files.append(data_to_send)
+            d = cv2.imread(data_to_send)
             data = pickle.dumps(d)
             data_to_send = {}
             data_to_send["command"] = command_to_send
@@ -94,20 +105,24 @@ def main():
             data_to_send["data"] = data
             data_to_send = pickle.dumps(data_to_send)
             client_socket.send(data_to_send)
-            data_received = client_socket.recv(constants.RECV_BUFFER_SIZE)        
-            print(data_received.decode(constants.ENCONDING_FORMAT))
-            command_to_send = input()              
-        else:        
-            client_socket.send(bytes(command_to_send,constants.ENCONDING_FORMAT))
-            data_received = client_socket.recv(constants.RECV_BUFFER_SIZE)        
+            data_received = client_socket.recv(constants.RECV_BUFFER_SIZE)
             print(data_received.decode(constants.ENCONDING_FORMAT))
             command_to_send = input()
-    
+        else:
+            client_socket.send(bytes(command_to_send,constants.ENCONDING_FORMAT))
+            data_received = client_socket.recv(constants.RECV_BUFFER_SIZE)
+            print(data_received.decode(constants.ENCONDING_FORMAT))
+            command_to_send = input()
+
+
+
     client_socket.send(bytes(command_to_send,constants.ENCONDING_FORMAT))
-    data_received = client_socket.recv(constants.RECV_BUFFER_SIZE)        
+    data_received = client_socket.recv(constants.RECV_BUFFER_SIZE)
     print(data_received.decode(constants.ENCONDING_FORMAT))
     print('Closing connection...BYE BYE...')
-    client_socket.close() 
+    client_socket.close()
+
+
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -11,57 +11,73 @@ import cv2
 import urllib.parse
 import threading
 
+
 files = {}
-        
 
 
 def splitFile(bytesFile, name):
+
+
     nameR = name
+    originalCode = bytesFile
     print(len(bytesFile))
     n_iteracions = len(bytesFile) / 1024
-    if isinstance(n_iteracions,float):
+    if isinstance(n_iteracions, float):
         n_iteracions = int(n_iteracions) + 1
     print(n_iteracions)
     chunks = []
     min_limit = 0
     max_limit = 1024
     names = []
-    #ACA ESCRIBO LOS BYTES EN EL ARCHIVO
-    for i in range (n_iteracions):
-        name = name + str(i)
+    counter = 0
+    name_cons = name
+    # ACA ESCRIBO LOS BYTES EN EL ARCHIVO
+    for i in range(n_iteracions):
+        print(name)
+        if counter == 1:
+            name = name_cons + '1'
+        else:
+            name = name_cons + str(counter)
         names.append(name)
-        file = open (name,'wb')
+        file = open(name, 'wb')
         file.write(bytesFile[min_limit:max_limit])
         chunks.append(bytesFile[min_limit:max_limit])
         file.close()
         min_limit = max_limit
         max_limit += 1024
-    files[nameR] = names
-    return chunks
-
-
+        counter += 1
+        files[nameR] = names
+        return chunks
 
 
 # Defining a socket object...
-server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = constants.IP_SERVER
-#cl1, [logo.jpg00, asusenas.jpg01]
+
+#Client1 = [logo.jpg0,...]
 clientAdresses = {}
-#CL1: IP, PUERTO SOCKET, PUERTO HTTP
+
+
 def main():
 
     print("***********************************")
+
+
     print("Server is running...")
-    print("Dir IP:",server_address )
+    print("Dir IP:", server_address)
     print("Port:", constants.PORT)
     server_execution()
-    
+
 # Handler for manage incomming clients conections...
 
-def handler_client_connection(client_connection,client_address):
+
+def handler_client_connection(client_connection, client_address):
+
+
     clientAdresses[client_address[0]] = []
     clientParts = {}
-    print(f'New incomming connection is coming from: {client_address[0]}:{client_address[1]}')
+    print(
+        f'New incomming connection is coming from: {client_address[0]}:{client_address[1]}')
     is_connected = True
     while is_connected:
         data_recevived = client_connection.recv(constants.RECV_BUFFER_SIZE)
@@ -69,7 +85,7 @@ def handler_client_connection(client_connection,client_address):
         remote_command = []
         if type(data_recevived) is dict:
             remote_command.append(data_recevived["command"])
-            remote_command.append(data_recevived["data"]) 
+            remote_command.append(data_recevived["data"])
             remote_command.append(data_recevived["name"])
             print(remote_command[0])
             command = remote_command[0]
@@ -77,7 +93,7 @@ def handler_client_connection(client_connection,client_address):
             remote_command = data_recevived.split()
             command = remote_command[0]
             print(command)
-        print (f'Data received from: {client_address[0]}:{client_address[1]}')        
+        print(f'Data received from: {client_address[0]}:{client_address[1]}')
         if (command == constants.QUIT):
             response = '200 BYE\n'
             client_connection.sendall(response.encode(constants.ENCONDING_FORMAT))
@@ -85,7 +101,7 @@ def handler_client_connection(client_connection,client_address):
         elif (command == constants.DOWNLOAD):
             bytesTotal = b''
             #conn = http.client.HTTPConnection("127.0.0.1",53000)
-            #requestHttp = "/Julian"
+            #requestHttp = "/JulianEllindo"
             #conn.request("GET", requestHttp)
             #firstResponse = conn.getresponse()
             #firstResponse = firstResponse.read()
@@ -96,82 +112,97 @@ def handler_client_connection(client_connection,client_address):
             #firstResponse = conn.getresponse()
             #firstResponse = firstResponse.read()
             #bytesTotal = bytesTotal + firstResponse
-            if(files[remote_command[2]] != ''):
+            print(clientAdresses)
+            if(files[remote_command[1]] != ''):
                 for client in clientAdresses.keys():
-                    found_file = [filename for filename in clientAdresses[client] if remote_command[2] in filename]
-                    if found_file != []:
-                        conn = http.client.HTTPConnection(client,53000)
-                        requestHttp = "/" + found_file
+                    chunk_request = ''
+                    for chunk in clientAdresses[client]:
+                        print(remote_command[1])
+                        print(chunk)
+                        print(remote_command[1] in chunk)
+                        print(remote_command[1] != chunk)
+                        if remote_command[1] in chunk and remote_command[1] != chunk:
+                            chunk_request = chunk
+
+                    if chunk_request != '':
+                        print(chunk_request)
+                        print("papi va a preguntar por este", chunk_request)
+                        conn = http.client.HTTPConnection(client, 53000)
+                        requestHttp = "/" + chunk_request
                         print("the request is ", requestHttp)
                         conn.request("GET", requestHttp)
                         firstResponse = conn.getresponse()
                         firstResponse = firstResponse.read()
                         bytesTotal = bytesTotal + firstResponse
                         print("message from client "+str(client)+str(firstResponse))
-            client_connection.sendall(bytesTotal)
+                        print("finished")
+                        print("lo que diga armando")
+                        print(bytesTotal)
+                client_connection.sendall(bytesTotal)
+
         elif (command == constants.SAVE):
             data = remote_command[1]
-            #cv2.imshow('Imagen',pickle.loads(data))
-            #cv2.waitKey(5000)
-            #cv2.destroyAllWindows()
+            # cv2.imshow('Imagen',pickle.loads(data))
+            # cv2.waitKey(5000)
+            # cv2.destroyAllWindows()
             #files[remote_command[2]] = client_address[0]
             chunks = splitFile(data, remote_command[2])
             count = 0
             for client in clientAdresses.keys():
-                conn = http.client.HTTPConnection(client,53000)    
-                headers = {'Content-type': 'application/octet-stream','fileName':remote_command[2]+str(count)}
-                file = open(remote_command[2]+str(count),"rb")
+                conn = http.client.HTTPConnection(client, 53000)
+                headers = {'Content-type': 'application/octet-stream',
+                           'fileName': remote_command[2]+str(count)}
+                file = open(remote_command[2]+str(count), "rb")
+                print('conectado')
                 data = file.read()
                 conn.request('POST', '/post', data, headers)
+                clientAdresses[client].append(remote_command[2]+str(count))
                 count += 1
-            clientAdresses[client_address[0]].append(remote_command[2]+str(count))
 
-            #conn = http.client.HTTPConnection("127.0.0.1",53000)
-            #file = open("logo.jpg0","rb")
-            #data = file.read()
-            #headers = {'Content-type': 'application/octet-stream','fileName':'Julian'}
-            #clientAdresses[client_address[0]].append("Julian")
-            #clientParts ["Julian"] = client_address[0]            
-            #conn.request('POST', '/post/', data, headers)
-            #count += 1
-            #conn = http.client.HTTPConnection("127.0.0.1",54000)
-            #file = open("logo.jpg01","rb")
-            #data = file.read()
-            #headers = {'Content-type': 'application/octet-stream','fileName':'Armando'}
-            #clientAdresses[client_address[0]].append("Armando")
-            #clientParts ["Armando"] = client_address[0]
-            #conn.request('POST', '/post/', data, headers)
-            #count += 1
-            print(files)
-            response = "200 OK\n"
-            client_connection.sendall(response.encode(constants.ENCONDING_FORMAT))
+#conn = http.client.HTTPConnection("127.0.0.1",53000)
+#file = open("logo.jpg0","rb")
+#data = file.read()
+#headers = {'Content-type': 'application/octet-stream','fileName':'Julian'}
+# clientAdresses[client_address[0]].append("Julian")
+#clientParts ["Julian"] = client_address[0]
+#conn.request('POST', '/post/', data, headers)
+#count += 1
+#conn = http.client.HTTPConnection("127.0.0.1",54000)
+#file = open("logo.jpg01","rb")
+#data = file.read()
+#headers = {'Content-type': 'application/octet-stream','fileName':'Armando'}
+# clientAdresses[client_address[0]].append("Armando")
+#clientParts ["Armando"] = client_address[0]
+#conn.request('POST', '/post/', data, headers)
+#count += 1
+                print(files)
+                response = "200 OK\n"
+                client_connection.sendall(response.encode(constants.ENCONDING_FORMAT))
         else:
             response = '400 BCMD\n\rCommand-Description: Bad command\n\r'
             client_connection.sendall(response.encode(constants.ENCONDING_FORMAT))
-    
-    print(f'Now, client {client_address[0]}:{client_address[1]} is disconnected...')
-    client_connection.close()
+        print(f'Now, client {client_address[0]}:{client_address[1]} is disconnected...')
+        client_connection.close()
 
-#Function to start server process...
+# Function to start server process...
+
+
 def server_execution():
-    tuple_connection = (server_address,constants.PORT)
+
+
+    tuple_connection = (socket.gethostname(), constants.PORT)
     server_socket.bind(tuple_connection)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    print ('Socket is bind to address and port...')
+    print('Socket is bind to address and port...')
     server_socket.listen(5)
     print('Socket is listening...')
     while True:
         client_connection, client_address = server_socket.accept()
-        client_thread = threading.Thread(target=handler_client_connection, args=(client_connection,client_address))
+        client_thread = threading.Thread(
+        target=handler_client_connection, args=(client_connection, client_address))
         client_thread.start()
     print('Socket is closed...')
     server_socket.close()
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
